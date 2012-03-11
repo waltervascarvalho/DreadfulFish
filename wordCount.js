ea7c955512e1dac546137590d88ef130c5261a49
Walter Carvalho <waltervascarvalho@gmail.com>
2012-03-11 17:33:01 +0000

    Word Count (Mostly)
    Word count functionality, now working.

--- wordCount.js
+++ wordCount.js
@@ -1,7 +1,7 @@
 $(document).ready(function()
 {
     //set max length
-    var max_length = 2000;
+    var max_length = 0;
  
     //load in max characters when page loads
     $("#counter").html(max_length);
@@ -12,7 +12,7 @@ $(document).ready(function()
  
 whenkeydown = function(max_length)
 {
-	max_length += 1;
+	max_length -= 1;
 	
     $("#input").unbind().keyup(function()
     {
@@ -20,25 +20,18 @@ whenkeydown = function(max_length)
         if(document.activeElement.id === "input")
         {
             //get the data in the field
-            var text = $(this).val();
+            var text = $(this).val().trim;
+			
  
             //set number of words.
-            var numofwords = this.value.split(' ').length;
+            var numofwords = this.value.split(/\S+/g).length;
  
-            //set the number of words left.
-            var words_left = max_length - numofwords;
- 
-            //check if we are still within our maximum number of characters or not
-            if(numofwords <= max_length)
-            {
-                //set the length of the text into the counter span
-                $("#counter").html("").html(words_left).css("color", "#000000");
-            }
-            else
-            {
-                //style numbers in red
-                $("#counter").html("").html(words_left).css("color", "#FF0000");
-            }
+            //set the number of total words.
+            var words_total = max_length + numofwords;
+
+			//set the total of the text into the counter span
+			$("#counter").html("").html(words_total).css("color", "#D3D3D3");
+
         }
     });
 }

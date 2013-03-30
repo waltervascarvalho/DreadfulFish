1be9e108bbae3ed838065a66b357f84f7bcc7b1b
Walter Carvalho <waltervascarvalho@gmail.com>
2013-03-30 19:20:22 +0000

    cleaning up

--- dreadfulfish.js
+++ dreadfulfish.js
@@ -2,16 +2,30 @@ var shorts = ["ain't","aren't","aren't","arent","basically","cannot","can't","co
 
 var longs = ["is not","are not","are not","are not","to put it simply","can not","can not","color","could not","did not","does not","do not","down load","well-known","had not","has not","have not","he would","he will","he is ","in the past","how did","how is","I would","I will","I am","I have","I wanted to say","I've got to","I must","I intend to","I intend to","make it better","was going to","is not","it is a","it is","it is","I have","let us","a long time ago","name is",",in all likelyhood,","oh","raining cats and dogs","are you","are you","she would","she is","she is","should not","that is","that is","there is","these're","those're","they are","at the end of the day","you are","your","you","want to","was not","we will","were not","we are","we have","what is","what is","where is","who is","will not","will not","would not","yes","yes","yes it","you would","you will","you are","you have"];
 
-
-
 function findReplace(matchList, switchList, text){
-	var changeCount=0;
-	for(var i=0; i<matchList.length;i++){
+	var changeCount = 0;
+	
+	for(var i = 0; i < matchList.length; i++){
 		// Escape the escape characters in RegExp builders.
 		var re = new RegExp("\\b"+matchList[i]+"\\b", "ig");
-		if(re.test(text)==true)
+		
+		if(re.test(text) == true)
 			changeCount++;
+		
 		text = text.replace(re,switchList[i]);
 	}
+
 	return text + " ["+changeCount+" changes.]";
 } 
+
+function shortToLong(){
+	var words = document.getElementById("input").value;
+	var result = findReplace(shorts, longs, words);
+	document.getElementById("output").value = result;
+}
+			
+function longToShort(){
+	var words = document.getElementById("input").value;
+	var result = findReplace(longs, shorts, words);
+	document.getElementById("output").value = result;
+}
--- wordCount.js
+++ wordCount.js
@@ -1,40 +1,37 @@
-$(document).ready(function()
-{
-    //set max length
-    var max_length = 0;
-
+$(document).ready(function() {
+	
+	//set max length
+	var max_length = 0;
 	var words_start;
  
-    //load in max characters when page loads
-    $("#counter").html(max_length);
- 
-    //run listen key press
-    whenkeydown(max_length);
+	//load in max characters when page loads
+	$("#counter").html(max_length);
+
+	//run listen key press
+	whenkeydown(max_length);
+
 });
  
-whenkeydown = function(max_length)
-{
+whenkeydown = function(max_length) {
+	
 	max_length -= 1;
 	
-    $("#input").unbind().keyup(function()
-    {
-        //check if the appropriate text area is being typed into
-        if(document.activeElement.id === "input")
-        {
-            //get the data in the field
-            var text = $(this).val().trim;
-			
- 
-            //set number of words.
-            numofwords = this.value.split(/\S+/g).length;
- 
-            //set the number of total words.
-            words_start = max_length + numofwords;
+	$("#input").unbind().keyup(function() {
+		//check if the appropriate text area is being typed into
+		if (document.activeElement.id === "input") {
+			//get the data in the field
+			var text = $(this).val().trim;
 
+			//set number of words.
+			numofwords = this.value.split(/\S+/g).length;
+
+			//set the number of total words.
+			words_start = max_length + numofwords;
+			
 			//set the total of the text into the counter span
 			$("#counter").html("").html(words_start).css("color", "#777");
-			
+	
 			return words_start;
-        }
-    });
+		}
+	});
 }
